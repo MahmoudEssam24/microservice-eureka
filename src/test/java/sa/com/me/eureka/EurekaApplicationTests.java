c0a3a2341002a7dcb44e11eb030465c18a1c9507
Mahmoud <mahmoudessam24@gmail.com>
2020-06-02 09:24:43 +0300

    Adding project files

---
+++ src/test/java/sa/com/me/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package sa.com.me.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
